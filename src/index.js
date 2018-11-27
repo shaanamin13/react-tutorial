9e4460d716634bd5bd3fc348bd88fe99df616e33
Shaan Amin <shaan.amin@igt.com>
2018-11-27 14:56:06 -0800

    moved handle click function

--- src/index.js
+++ src/index.js
@@ -20,19 +20,6 @@ class Board extends React.Component {
     );
   }
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext
-    });
-  }
-
   render() {
     return (
       <div>
@@ -68,6 +55,26 @@ class Game extends React.Component {
       xIsNext: true
     };
   }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
     const history = this.state.history;
     const current = history[history.length - 1];
